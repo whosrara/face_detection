3888b0092715da19475065760c5e12f015d2ff9a
baagas0 <baagas0@gmail.com>
2024-03-22 23:17:24 +0700

    test

--- face_haarcascade.py
+++ face_haarcascade.py
@@ -1,6 +1,6 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
 img = cv2.imread('jeni.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
